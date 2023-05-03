cf7ad032bf5125f76acf7e078eceab5b3e356e75
RonaldCabral <cabralronald850@gmail.com>
2023-05-03 01:02:12 -0400

    Complete

--- script.js
+++ script.js
@@ -1,51 +1,64 @@
-const url = 'https://openai.com/blog/chatgpt'
-async function scraper (url) {
-    const puppeteer = require('puppeteer');
-    const mysql = require('mysql2');
-    
-    //Configuring the connection to the database
-    const connection = mysql.createConnection({
-      host: '',
-      user: '',
-      password: '',
-      database: '',
-    });
-    
-    //Function to insert data into the database
-    function insertDataContent(content) {
-      const query = `INSERT INTO data (content) VALUES ('${content}')`;
-      connection.query(query, (error, results, fields) => {
-        if (error) {
-          console.error(error);
-        }
-        console.log('Data inserted correctly.');
-      });
+const puppeteer = require('puppeteer');
+const mysql = require('mysql');
+
+// Set up MySQL connection
+const connection = mysql.createConnection({
+  host: '',
+  user: '',
+  password: '',
+  database: ''
+});
+
+// Connect to MySQL
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected to MySQL database');
+});
+
+// Function to insert data into MySQL
+function insertData(title, content) {
+  const sql = 'INSERT INTO data (title, content) VALUES (?, ?)';
+  const values = [title, content];
+  connection.query(sql, values, function(err, result) {
+    if (err) throw err;
+    console.log('Data inserted successfully');
+  });
+}
+
+// Function to extract data from webpage
+async function extractData(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  // Get all divs on the page
+  const divs = await page.$$('div');
+
+  for (const div of divs) {
+    // Check if div has a title
+    const title = await page.evaluate(() => Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6')).map(Element => Element.textContent));
+
+    // Get all paragraphs in the div
+    const paragraphs = await div.$$('p');
+    let content = '';
+
+    // Combine all paragraphs into a single string
+    for (const paragraph of paragraphs) {
+      const text = await paragraph.evaluate(element => element.textContent.trim());
+      content += text + ' ';
     }
-    
-    //Main function (where the magic happens)
-    async function scrape(link) {
-  
-      //We start an instance of Puppeteer
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-    
-      //Go to the page
-      await page.goto(url);
-    
-      //Get the data
-     const content = await page.evaluate(() => Array.from(document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, li')).map(Element => Element.textContent));
-      
-      //insert the data into the database
-      for (let i = 0; i < content.length; i++) {
-        insertDataContent(content[i]);
-      }
-    
-      // Close th instance 
-      await browser.close();
+
+    // If no title, save paragraphs as title
+    if (!title) {
+      title = content.substring(0, 50) + '...';
     }
-    
-    // exec 
-    scrape(url)
+
+    // Insert data into MySQL
+    insertData(title, content);
   }
 
-scraper(url)
+  await browser.close();
+}
+
+// Example usage
+extractData('');
